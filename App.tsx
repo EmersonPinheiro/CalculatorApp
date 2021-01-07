f8fa6dd069ab68d7f6e93ffd347c48310555945e
Emerson Pinheiro <e_pms@hotmail.com>
2021-01-07 19:59:01 -0300

    App begin

--- App.tsx
+++ App.tsx
@@ -1,109 +1,66 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import React, {useState, useCallback, useEffect} from 'react';
+import {Button, SafeAreaView, StyleSheet, Text} from 'react-native';
 
-import React from 'react';
-import {SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar} from 'react-native';
+const DIGIT_LIMIT = 8;
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-declare const global: {HermesInternal: null | {}};
+type MathOperator = '+' | '-' | '/' | '*';
 
 const App = () => {
+  const [currentNumberDigits, setCurrentNumberDigits] = useState<string>('');
+  const [currentOperator, setCurrentOperator] = useState<MathOperator>();
+
+  //TODO: Numpad input
+  //TODO: Display
+
+  //TODO: Operation function
+  const calculate = useCallback(() => {}, []);
+
+  //TODO: onChangeCurrentNumber
+  const onChangeCurrentNumber = useCallback(
+    (digit: string) => {
+      if (currentNumberDigits.length < DIGIT_LIMIT) {
+        setCurrentNumberDigits(currentNumberDigits + digit);
+      }
+    },
+    [currentNumberDigits],
+  );
+
+  //TODO: onChangeCurrentOperator
+  const onChangeCurrentOperator = useCallback((operator: MathOperator) => {
+    if (currentNumberDigits.length) {
+      cu
+    }
+  }, []);
+
   return (
     <>
-      <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic" style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change this screen and then
-                come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
+        {/**DISPLAY */}
+        <Text>{currentNumberDigits}</Text>
+
+        {/**NUMPAD */}
+        <Button onPress={() => onChangeCurrentNumber('1')} title="1" />
+        <Button onPress={() => onChangeCurrentNumber('2')} title="2" />
+        <Button onPress={() => onChangeCurrentNumber('3')} title="3" />
+        <Button onPress={() => onChangeCurrentNumber('4')} title="4" />
+        <Button onPress={() => onChangeCurrentNumber('5')} title="5" />
+        <Button onPress={() => onChangeCurrentNumber('6')} title="6" />
+        <Button onPress={() => onChangeCurrentNumber('7')} title="7" />
+        <Button onPress={() => onChangeCurrentNumber('8')} title="8" />
+        <Button onPress={() => onChangeCurrentNumber('9')} title="9" />
+        <Button onPress={() => onChangeCurrentNumber('0')} title="0" />
+
+        {/**OPERATORS */}
+        <Button onPress={() => onChangeCurrentNumber('+')} title="+" />
+        <Button onPress={() => onChangeCurrentNumber('-')} title="-" />
+        <Button onPress={() => onChangeCurrentNumber('/')} title="/" />
+        <Button onPress={() => onChangeCurrentNumber('*')} title="*" />
+        <Button onPress={() => onChangeCurrentNumber('=')} title="=" />
       </SafeAreaView>
     </>
   );
 };
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
